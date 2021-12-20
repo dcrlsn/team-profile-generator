efd10a30c7fc5a0601e9b1aa37efae8fb0aa5e51
89209062+dcrlsn@users.noreply.github.com <c4r150n@gmail.com>
2021-12-19 20:42:51 -0500

    styling

--- src/generateTeam.js
+++ src/generateTeam.js
@@ -82,9 +82,12 @@ module.exports = team => {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>My Team</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
   <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 <body class="bg-secondary">
